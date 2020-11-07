1b1d369d6bd50b25c61c299d9f70ca1ff3dd2a41
Aleksey Yurash <ayurash@me.com>
2020-11-07 17:42:16 +0300

    Контейнер сервисов, HostBuilder

--- PManager.WPF/App.xaml.cs
+++ PManager.WPF/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace PManager.WPF
 {
@@ -13,5 +15,27 @@ namespace PManager.WPF
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Host;
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
