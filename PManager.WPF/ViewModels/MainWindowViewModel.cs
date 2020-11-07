419f1aa016024623cf663d8ef272f5efd0ac0787
Aleksey Yurash <ayurash@me.com>
2020-11-07 16:50:41 +0300

    Added: ViewModel, MainWindowViewModel

---
+++ PManager.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PManager.WPF.ViewModels.Base;
+
+namespace PManager.WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
---
+++ PManager.WPFpre/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PManager.WPFpre.ViewModels.Base;
+
+namespace PManager.WPFpre.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+    }
+}
