79771ce0cd0dc70a0b95a1b280915c0a733039e4
Aleksey Yurash <ayurash@me.com>
2020-11-07 16:14:47 +0300

    Add Project PManager.CL

---
+++ PManager.CL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PManager.CL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
