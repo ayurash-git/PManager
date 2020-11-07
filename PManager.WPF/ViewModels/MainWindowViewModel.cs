147344b65109fdc16eac4f516bc83e401371162c
Aleksey Yurash <ayurash@me.com>
2020-11-07 17:12:16 +0300

    Тестирование работы ViewModel

--- PManager.WPF/ViewModels/MainWindowViewModel.cs
+++ PManager.WPF/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,17 @@ namespace PManager.WPF.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок Окна
+
+        private string _title = "Project Manager";
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get => _title; 
+            set => Set(ref _title, value);
+        }
+
+        #endregion
 
     }
 }
